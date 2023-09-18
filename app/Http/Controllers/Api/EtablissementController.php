85b238cd8ee171bc60ae41126ea66b0e1b86587c
TCHOUKOUAHA Boris Gautier <btchoukouaha@univ-catho-sjd.com>
2023-09-18 14:06:34 +0100

    update distance

--- app/Http/Controllers/Api/EtablissementController.php
+++ app/Http/Controllers/Api/EtablissementController.php
@@ -643,7 +643,7 @@ class EtablissementController extends BaseController
         } else {
             $etablissement->isFavoris = false;
         }
-        $etablissement->distance;
+        $etablissement->distance = $etablissement->distance;
         $etablissement->isopen = $this->checkIfEtablissementIsOpen($etablissement->id);
         $etablissement->moyenne = $this->getMoyenneRatingByEtablissmeent($etablissement->id);
         $etablissement->avis = $this->getCommentNumberByEtablissmeent($etablissement->id);
